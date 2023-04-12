d70b3ee39bd4b7b0e5ef12e6e714f1b79e65df22
Saubhagya <saubhagyagupta8@gmail.com>
2023-04-12 15:55:11 +0530

    created second function and called it using p object

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Product p = new Product();
         int x=p.product(3,4);
 //
-//        int z=p.product(3,4,5);
+        int z=p.product(3,4,5);
 //
 //        double y=p.product(3.5,4.5);
     }
@@ -15,9 +15,9 @@ class Product{
     public int product(int x, int y){
         return x;
     }
-//    public int product(int x, int y, int z) {
-//        return z;
-//    }
+    public int product(int x, int y, int z) {
+        return z;
+    }
 //
 //    public double product(double x, double y) {
 //        return y;
