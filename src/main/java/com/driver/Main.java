3a9657398b771c8f1a627f375ea556d80fbce9ee
Saubhagya <saubhagyagupta8@gmail.com>
2023-04-12 15:52:25 +0530

    created firs object and called first function

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
         int x=p.product(3,4);
-
-        int z=p.product(3,4,5);
-
-        double y=p.product(3.5,4.5);
-
-
+//
+//        int z=p.product(3,4,5);
+//
+//        double y=p.product(3.5,4.5);
     }
 }
 
@@ -17,11 +15,11 @@ class Product{
     public int product(int x, int y){
         return x;
     }
-    public int product(int x, int y, int z) {
-        return z;
-    }
-
-    public double product(double x, double y) {
-        return y;
-    }
+//    public int product(int x, int y, int z) {
+//        return z;
+//    }
+//
+//    public double product(double x, double y) {
+//        return y;
+//    }
 }
